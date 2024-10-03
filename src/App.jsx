98f6a791c221e00d3a44aac809ac315afd2c0d3d
Farid Ullah <shahzad572a@gmail.com>
2024-10-03 06:10:57 -0700

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
- 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardMain from './components/Dashboard/DashboardMain';
-// import Navbar from './components/Layout/Navbar';
- 
+
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Router>
-               {/* <Navbar/> */}
-        <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */} 
-        
-          <Route path="/*" element={<DashboardMain/>}/>
-
-
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<DashboardMain />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Dashboard/DashboardMain.jsx
+++ src/components/Dashboard/DashboardMain.jsx
@@ -3,41 +3,29 @@ import Sidebar from '../../pages/Sidebar';
 import { Routes, Route } from 'react-router-dom';
 import Tasks from '../../pages/Tasks';
 import LoginPage from '../Auth/Login';
- import ProtectedRoute from '../../utils/ProtectedRoute';
+import ProtectedRoute from '../../utils/ProtectedRoute';
 import AllTasks from '../../pages/AllTasks';
 import ManageUsers from '../Auth/ManageUsers';
 import TeamMembers from '../../pages/TeamMembers';
 import Logout from '../Auth/Logout';
 import RegisterPage from '../Auth/RegisterPage';
- 
+
 const DashboardMain = () => {
   const managerId = localStorage.getItem('userInfoId');
+
   return (
     <div className="flex h-screen">
-      {/* Sidebar */}
       <Sidebar />
 
       <div className="flex flex-col w-full overflow-auto">
-        
-        {/* <Header /> */}
-
-     
         <Routes>
-          
-        
-          <Route path="/task" element={<Tasks />} /> 
-          <Route path="/login" element={<LoginPage />} /> 
-          <Route path="/logout" element={<Logout />} /> 
-          <Route path="/register" element={<RegisterPage />} /> 
-
-
-         
-          {/* <Route path="/all-tasks" element={<AllTasks />} />  */}
-          <Route path="/users" element={<ProtectedRoute element={ManageUsers} allowedRoles={['admin']} />} /> 
+          <Route path="/task" element={<Tasks />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/users" element={<ProtectedRoute element={ManageUsers} allowedRoles={['admin']} />} />
           <Route path="/all-tasks" element={<ProtectedRoute element={AllTasks} allowedRoles={['admin']} />} />
-
-          {/* <Route path="/team" element={<TeamMembers managerId={managerId}/>} />  */}
-          <Route path="/team" element={<ProtectedRoute element={TeamMembers}  managerId={managerId}  allowedRoles={['manager']}/>} />
+          <Route path="/team" element={<ProtectedRoute element={TeamMembers} managerId={managerId} allowedRoles={['manager']} />} />
         </Routes>
       </div>
     </div>
